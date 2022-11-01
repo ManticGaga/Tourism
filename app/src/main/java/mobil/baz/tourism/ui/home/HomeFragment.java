6a6cdbb458b98667a1315ec4f2a8458f7c4979bc
Your Name <ManticGaga@gmail.com>
2022-11-02 00:24:32 +0300

    ADDED STARTSCREEN

--- app/src/main/java/mobil/baz/tourism/ui/home/HomeFragment.java
+++ app/src/main/java/mobil/baz/tourism/ui/home/HomeFragment.java
@@ -1,46 +1,43 @@
 package mobil.baz.tourism.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import org.jetbrains.annotations.NotNull;
 
+import mobil.baz.tourism.MainActivity;
+import mobil.baz.tourism.R;
+import mobil.baz.tourism.TurViewmodel;
+import mobil.baz.tourism.databinding.ActivityMainBinding;
 import mobil.baz.tourism.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment  {
+public class HomeFragment extends AppCompatActivity{
 
     private FragmentHomeBinding binding;
-    Button mButton;
-
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-
-
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-
+    public void mainbtn(View view) {
+        Intent intent = new Intent(HomeFragment.this, MainActivity.class);
+        startActivity(intent);
     }
-
     @Override
-    public void onViewCreated(@NotNull View view, @Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_home);
 
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
+
+
+
 }
