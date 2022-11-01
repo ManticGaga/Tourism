20834258f11750a9e4baa8f37ea218f98ee95fb2
Your Name <ManticGaga@gmail.com>
2022-11-02 00:48:18 +0300

    First practice

--- app/src/main/java/mobil/baz/tourism/ui/home/HomeFragment.java
+++ app/src/main/java/mobil/baz/tourism/ui/home/HomeFragment.java
@@ -1,43 +1,46 @@
 package mobil.baz.tourism.ui.home;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import org.jetbrains.annotations.NotNull;
 
-import mobil.baz.tourism.MainActivity;
-import mobil.baz.tourism.R;
-import mobil.baz.tourism.TurViewmodel;
-import mobil.baz.tourism.databinding.ActivityMainBinding;
 import mobil.baz.tourism.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends AppCompatActivity{
+public class HomeFragment extends Fragment  {
 
     private FragmentHomeBinding binding;
-    public void mainbtn(View view) {
-        Intent intent = new Intent(HomeFragment.this, MainActivity.class);
-        startActivity(intent);
+    Button mButton;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
     }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home);
+    public void onViewCreated(@NotNull View view, @Nullable Bundle savedInstanceState) {
 
     }
 
-
-
-
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
